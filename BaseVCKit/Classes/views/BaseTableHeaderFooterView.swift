942776a73e5237b6915b0d68cea3ecae9f47b990
seongkyu-sim <seongkyu.sim@gmail.com>
2017-06-02 16:39:20 +0900

    fix access authority, ver 0.4.3

--- BaseVCKit/Classes/views/BaseTableHeaderFooterView.swift
+++ BaseVCKit/Classes/views/BaseTableHeaderFooterView.swift
@@ -8,40 +8,40 @@
 
 import UIKit
 
-public class BaseTableHeaderFooterView: UIView {
+open class BaseTableHeaderFooterView: UIView {
 
-    public lazy var contentView: UIView = { [unowned self] in
+    open lazy var contentView: UIView = { [unowned self] in
         let v = UIView()
         self.addSubview(v)
         return v
         }()
 
-    public override init(frame: CGRect) { // for using CustomView in code
+    open override init(frame: CGRect) { // for using CustomView in code
         super.init(frame: frame)
 
         self.commonInit()
     }
 
-    public required init?(coder aDecoder: NSCoder) { // for using CustomView in IB
+    open required init?(coder aDecoder: NSCoder) { // for using CustomView in IB
         super.init(coder: aDecoder)
 
         self.commonInit()
     }
 
-    public override func layoutSubviews() {
+    open override func layoutSubviews() {
         super.layoutSubviews()
 
         self.frame.size = contentView.frame.size
     }
 
-    public override func willMove(toSuperview newSuperview: UIView?) {
+    open override func willMove(toSuperview newSuperview: UIView?) {
         super.willMove(toSuperview: newSuperview)
 
         setNeedsLayout()
         layoutIfNeeded()
     }
 
-    public func commonInit() {
+    open func commonInit() {
         backgroundColor = UIColor.white
         configureConstraints()
     }
@@ -49,7 +49,7 @@ public class BaseTableHeaderFooterView: UIView {
 
     // MARK: - Layout
 
-    public func configureConstraints() {
+    open func configureConstraints() {
         contentView.snp.makeConstraints { (make) in
             make.top.left.right.equalToSuperview()
         }
